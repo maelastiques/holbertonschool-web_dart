fe7fb0f5679dae1714fe2c7935b5f4cb8d4db793
maelastiques <mael.cuny@gmail.com>
2026-02-24 16:50:08 +0100

    corrected projects

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,7 +1,7 @@
 void main() {
   double number = 3.14159;
   double parsed = double.parse('3.14159');
-  if (number == 3.14159 && parsed != 3.14159) {
+  if (number == 3.14159) {
     number = -98.12;
   }
   print('Double: ${number.toStringAsFixed(2)}\n');
